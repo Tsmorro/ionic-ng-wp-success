2d37fcd3d4c3c0cdf897a29ce752d4398e9e61c4
tsmorro <tsmorro@gmail.com>
2019-04-16 09:21:25 -0400

    Stub out the DataService

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,26 +1,30 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
-
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
+      imports: [
+        HttpClientModule
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .compileComponents();
   }));
-
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  endpointURL: 'http://tmorrow.design/wp-json/'
 };
